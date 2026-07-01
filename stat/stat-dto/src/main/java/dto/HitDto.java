ce357a10cf783d51d9151c3577cf12f6576bf9b8
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2026-07-01 21:09:19 +0300

    Общие DTO для сервиса статистики - устранение замечаний ревью

--- stat/stat-dto/src/main/java/dto/HitDto.java
+++ stat/stat-dto/src/main/java/dto/HitDto.java
@@ -2,17 +2,22 @@ package dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
 
 public class HitDto {
-    private Long id;
 
     @NotBlank
     private String app;
@@ -20,9 +25,11 @@ public class HitDto {
     @NotBlank
     private String uri;
 
-    @NotBlank String ip;
-
     @NotBlank
+    private String ip;
+
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private String hit_timestamp;
+    @PastOrPresent(message = "timestamp не должен быть в будущем")
+    private LocalDateTime hit_timestamp;
 }
--- stat/stat-dto/src/main/java/dto/ViewStatsDto.java
+++ stat/stat-dto/src/main/java/dto/ViewStatsDto.java
@@ -3,17 +3,17 @@ package dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class ViewStatsDto {
-    @NotBlank
     private String app;
-
-    @NotBlank
     private String uri;
-
-    @NotNull
     private Long hits;
 }
