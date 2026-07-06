50f7900867c797e0eafa617f1cafb7e7118a1a36
demidov.ko <demidov.kod@yandex.ru>
2026-07-07 00:29:54 +0300

    Добавлены модель и dto для события и локации

---
+++ main-service/src/main/java/evm/main/event/model/EventState.java
@@ -0,0 +1,8 @@
+package evm.main.event.model;
+
+// Жизненный цикл события
+public enum EventState {
+    PENDING,    // создано, ожидает публикации
+    PUBLISHED,  // опубликовано администратором
+    CANCELED    // отменено администратором или инициатором
+}
