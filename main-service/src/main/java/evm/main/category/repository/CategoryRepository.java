0b8707aa9ba08090348adf2a81c0c0a0caae9557
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2026-07-14 00:04:06 +0300

    Категории. Устранение замечаний ревью.

--- main-service/src/main/java/evm/main/category/repository/CategoryRepository.java
+++ main-service/src/main/java/evm/main/category/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import evm.main.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
 }
