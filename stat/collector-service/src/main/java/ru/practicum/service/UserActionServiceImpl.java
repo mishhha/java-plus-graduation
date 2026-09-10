e7f5ab28ab881d7f82623cd3b010763bc30a2422
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-09-10 22:40:26 +0300

    fix: убрать неактивный import Instant

--- stat/collector-service/src/main/java/ru/practicum/service/UserActionServiceImpl.java
+++ stat/collector-service/src/main/java/ru/practicum/service/UserActionServiceImpl.java
@@ -12,8 +12,6 @@ import ru.practicum.ewm.stats.proto.collector.ActionTypeProto;
 import ru.practicum.ewm.stats.proto.collector.UserActionControllerGrpc;
 import ru.practicum.ewm.stats.proto.collector.UserActionProto;
 
-import java.time.Instant;
-
 @Slf4j
 @GrpcService
 @RequiredArgsConstructor
